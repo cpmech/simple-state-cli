9c430ad7d90a4037b957a7e752d50590dde5e36d
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-09-24 17:19:49 +1000

    Improve tests

--- src/store/data/__tests__/Data.test.ts
+++ src/store/data/__tests__/Data.test.ts
@@ -2,27 +2,31 @@ import { Data } from '../Data';
 import { newStateData } from '../types';
 
 class Notifier {
-  names: string[] = [];
-  observers = [() => this.names.push('bender'), () => this.names.push('leela')];
+  counter: number[] = [];
+  observers = [() => this.counter.push(this.counter.length)];
   onChange = () => this.observers.forEach(observer => observer());
 }
 
+const notifier = new Notifier();
+const data = new Data(notifier.onChange);
+const refState = newStateData();
+
+beforeEach(() => {
+  notifier.counter = [];
+  data.reset();
+});
+
 describe('Data', () => {
-  const notifier = new Notifier();
-  const data = new Data(notifier.onChange);
-
-  beforeEach(() => data.reset());
-
   describe('state', () => {
     it('should be properly initialized', () => {
-      expect(data.state).toEqual({ ...newStateData() });
+      expect(data.state).toEqual(refState);
     });
   });
 
   describe('constructor', () => {
     it('should bind to the correct onChange function', () => {
       data.onChange();
-      expect(notifier.names).toEqual(['bender', 'leela']);
+      expect(notifier.counter).toEqual([0]);
     });
   });
 
@@ -31,7 +35,7 @@ describe('Data', () => {
       data.setStringField('outputDirectory', '/tmp/just-testing');
       expect(data.state.outputDirectory).toBe('/tmp/just-testing');
       data.reset();
-      expect(data.state.outputDirectory).toBe('/tmp/simple-state/store');
+      expect(data.state.outputDirectory).toBe(refState.outputDirectory);
     });
   });
 
@@ -39,14 +43,12 @@ describe('Data', () => {
     it('should get the right values', () => {
       expect(data.getBooleanField('someBoolean')).toBeTruthy();
       expect(data.getNumberField('someNumber')).toBe(123);
-      expect(data.getStringField('outputDirectory')).toBe('/tmp/simple-state/store');
+      expect(data.getStringField('outputDirectory')).toBe(refState.outputDirectory);
       expect(data.getModuleNamesArray()).toEqual(['auth', 'user']);
     });
 
     it('should throw error on wrong fieldName', () => {
-      expect(() => data.getStringField('__inexistent__')).toThrowError(
-        'cannot find __inexistent__',
-      );
+      expect(() => data.getStringField('__none__')).toThrowError('cannot find __none__');
     });
 
     it('should throw error on wrong type', () => {
@@ -58,32 +60,51 @@ describe('Data', () => {
 
   describe('setters', () => {
     it('should set the right values', () => {
+      data.setBooleanField('someBoolean', false);
+      data.setNumberField('someNumber', 666);
       data.setStringField('outputDirectory', '/tmp/just-testing');
+      expect(data.state.someBoolean).toBeFalsy();
+      expect(data.state.someNumber).toBe(666);
       expect(data.state.outputDirectory).toBe('/tmp/just-testing');
+      expect(notifier.counter).toEqual([0, 1, 2]);
       data.pushModuleNames('hello world another one');
       expect(data.state.moduleNames).toBe('auth user hello world another one');
+      expect(notifier.counter).toEqual([0, 1, 2, 3]);
       data.removeModuleName('another');
       expect(data.state.moduleNames).toBe('auth user hello world one');
+      expect(notifier.counter).toEqual([0, 1, 2, 3, 4]);
     });
 
     it('should throw error on wrong fieldName', () => {
-      expect(() => data.setStringField('__inexistent__', 'value')).toThrowError(
-        'cannot find __inexistent__',
-      );
+      expect(() => data.setBooleanField('__none__', false)).toThrowError('cannot find __none__');
+      expect(() => data.setNumberField('__none__', 0)).toThrowError('cannot find __none__');
+      expect(() => data.setStringField('__none__', 'value')).toThrowError('cannot find __none__');
     });
 
     it('should throw error on wrong type', () => {
-      expect(() => data.setStringField('someNumber', 'value')).toThrowError(
+      expect(() => data.setBooleanField('outputDirectory', false)).toThrowError(
+        'type of outputDirectory is incorrect',
+      );
+      expect(() => data.setNumberField('someBoolean', 0)).toThrowError(
+        'type of someBoolean is incorrect',
+      );
+      expect(() => data.setStringField('someNumber', 'v')).toThrowError(
         'type of someNumber is incorrect',
       );
     });
 
     it('should call onChange', () => {
-      notifier.names = [];
-      data.setBooleanField('someBoolean', true);
-      data.setNumberField('someNumber', 1);
+      data.setBooleanField('someBoolean', false);
+      data.setNumberField('someNumber', 666);
       data.setStringField('outputDirectory', '/tmp/another-test');
-      expect(notifier.names).toEqual(['bender', 'leela', 'bender', 'leela', 'bender', 'leela']);
+      expect(notifier.counter).toEqual([0, 1, 2]);
+    });
+
+    it('should not call onChange', () => {
+      data.setBooleanField('someBoolean', true);
+      data.setNumberField('someNumber', 123);
+      data.setStringField('outputDirectory', refState.outputDirectory);
+      expect(notifier.counter).toEqual([]);
     });
   });
 });
