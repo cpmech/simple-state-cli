3813f82a4e52563bfd796088ba870ea703293037
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-11-15 13:51:01 +1000

    Simplofy generated tests

--- src/templates/genModuleTest.ts
+++ src/templates/genModuleTest.ts
@@ -3,20 +3,9 @@ import mustache from 'mustache';
 const template = `import { {{klass}} } from '../{{klass}}';
 import { newState{{klass}} } from '../types';
 
-class Notifier {
-  counter: number = 0;
-  observers = [() => this.counter++];
-  onChange = () => this.observers.forEach(observer => observer());
-}
-
-const notifier = new Notifier();
-const {{name}} = new {{klass}}(notifier.onChange);
+const onChange = jest.fn();
 const refState = newState{{klass}}();
-
-beforeEach(() => {
-  notifier.counter = 0;
-  {{name}}.resetWithoutCallingOnChange();
-});
+const {{name}} = new {{klass}}(onChange);
 
 describe('{{klass}}', () => {
   describe('state', () => {
@@ -28,78 +17,38 @@ describe('{{klass}}', () => {
   describe('constructor', () => {
     it('should bind to the correct onChange function', () => {
       {{name}}.onChange();
-      expect(notifier.counter).toBe(1);
-    });
-  });
-
-  describe('reset', () => {
-    it('should clear the state to the original values', () => {
-      {{name}}.setBooleanField('someBoolean', true);
-      expect({{name}}.state.someBoolean).toBeTruthy();
-      {{name}}.resetWithoutCallingOnChange();
-      expect({{name}}.state.someBoolean).toBeFalsy();
+      expect(onChange).toBeCalledTimes(1);
     });
   });
 
   describe('getters', () => {
     it('should get the right values', () => {
-      expect({{name}}.getBooleanField('someBoolean')).toBeFalsy();
-      expect({{name}}.getNumberField('someNumber')).toBe(0);
-      expect({{name}}.getStringField('someString')).toBe(refState.someString);
-    });
-
-    it('should throw error on wrong fieldName', () => {
-      expect(() => {{name}}.getStringField('__none__')).toThrowError('cannot find __none__');
-    });
-
-    it('should throw error on wrong type', () => {
-      expect(() => {{name}}.getStringField('someNumber')).toThrowError(
-        'type of someNumber is incorrect',
-      );
+      expect({{name}}.getStringField('role')).toBe('NADA');
+      expect(onChange).toBeCalledTimes(1);
     });
   });
 
   describe('setters', () => {
-    it('should set the right values', () => {
-      {{name}}.setBooleanField('someBoolean', true);
-      {{name}}.setNumberField('someNumber', 666);
-      {{name}}.setStringField('someString', 'just-testing');
-      expect({{name}}.state.someBoolean).toBeTruthy();
-      expect({{name}}.state.someNumber).toBe(666);
-      expect({{name}}.state.someString).toBe('just-testing');
-      expect(notifier.counter).toBe(3);
+    it('should set the right values and call onChange', () => {
+      expect({{name}}.state.role).toBe('NADA');
+      {{name}}.setStringField('role', 'TESTER');
+      expect({{name}}.state.role).toBe('TESTER');
+      expect(onChange).toBeCalledTimes(2);
     });
 
-    it('should throw error on wrong fieldName', () => {
-      expect(() => {{name}}.setBooleanField('__none__', true)).toThrowError('cannot find __none__');
-      expect(() => {{name}}.setNumberField('__none__', 123)).toThrowError('cannot find __none__');
-      expect(() => {{name}}.setStringField('__none__', 'value')).toThrowError('cannot find __none__');
+    it('should not call onChange if there is no difference', () => {
+      expect({{name}}.state.role).toBe('TESTER');
+      {{name}}.setStringField('role', 'TESTER');
+      expect({{name}}.state.role).toBe('TESTER');
+      expect(onChange).toBeCalledTimes(2);
     });
+  });
 
-    it('should throw error on wrong type', () => {
-      expect(() => {{name}}.setBooleanField('someString', false)).toThrowError(
-        'type of someString is incorrect',
-      );
-      expect(() => {{name}}.setNumberField('someBoolean', 0)).toThrowError(
-        'type of someBoolean is incorrect',
-      );
-      expect(() => {{name}}.setStringField('someNumber', 'v')).toThrowError(
-        'type of someNumber is incorrect',
-      );
-    });
-
-    it('should call onChange', () => {
-      {{name}}.setBooleanField('someBoolean', true);
-      {{name}}.setNumberField('someNumber', 666);
-      {{name}}.setStringField('someString', 'another-test');
-      expect(notifier.counter).toBe(3);
-    });
-
-    it('should not call onChange', () => {
-      {{name}}.setBooleanField('someBoolean', false);
-      {{name}}.setNumberField('someNumber', 0);
-      {{name}}.setStringField('someString', refState.someString);
-      expect(notifier.counter).toBe(0);
+  describe('reset', () => {
+    it('should clear the state to the original values', () => {
+      {{name}}.resetWithoutCallingOnChange();
+      expect({{name}}.state).toEqual(refState);
+      expect(onChange).toBeCalledTimes(2);
     });
   });
 });
--- src/templates/genStoreTest.ts
+++ src/templates/genStoreTest.ts
@@ -3,107 +3,54 @@ import mustache from 'mustache';
 const template = `import { Store } from '../Store';
 import { newState{{klass}} } from '../{{name}}';
 
-class Notifier {
-  counter: number = 0;
-  observers = [() => this.counter++];
-  onChange = () => this.observers.forEach(observer => observer());
-}
-
-const notifier = new Notifier();
-const store = new Store();
+const observer = jest.fn();
 const refState = newState{{klass}}();
-
-beforeEach(() => {
-  notifier.counter = 0;
-  store.reset(false);
-});
+const store = new Store();
 
 describe('Store', () => {
   describe('constructor', () => {
     it('should bind to the correct onChange function', () => {
-      store.subscribe(notifier.onChange, 'notifier');
+      store.subscribe(observer, 'store-test-observer');
       store.{{name}}.onChange();
-      expect(notifier.counter).toEqual(1);
+      expect(observer).toBeCalledTimes(1);
     });
   });
 
   describe('state', () => {
     it('should be properly initialized', () => {
       expect(store.{{name}}.state).toEqual(refState);
-      expect(notifier.counter).toBe(0);
+      expect(observer).toBeCalledTimes(1);
     });
   });
 
   describe('observers', () => {
     it('should call observers', () => {
-      store.{{name}}.setBooleanField('someBoolean', true);
-      expect(notifier.counter).toBe(1);
+      store.{{name}}.setStringField('role', 'TESTER');
+      expect(observer).toBeCalledTimes(2);
     });
 
     it('should properly unsubscribe observers', () => {
-      let called = 0;
-      const unsubscribe = store.subscribe(() => called++, 'singleObserver');
-      store.{{name}}.setBooleanField('someBoolean', true);
-      expect(called).toBe(1);
+      const temporaryObserver = jest.fn();
+      const unsubscribe = store.subscribe(temporaryObserver, 'store-test-temporary-observer');
+      store.{{name}}.setStringField('role', 'ADMIN');
+      expect(temporaryObserver).toBeCalledTimes(1);
+      expect(observer).toBeCalledTimes(3);
       unsubscribe();
-      expect(called).toBe(1);
-      store.{{name}}.setBooleanField('someBoolean', true);
-      expect(called).toBe(1);
-    });
-
-    it('should properly unsubscribe all observers', () => {
-      const st = new Store();
-      const obs1 = jest.fn();
-      const obs2 = jest.fn();
-      const obs3 = jest.fn();
-      const obs4 = jest.fn();
-      const unsubscribe1 = st.subscribe(obs1, 'obs1');
-      const unsubscribe2 = st.subscribe(obs2, 'obs2');
-      const unsubscribe3 = st.subscribe(obs3, 'obs3');
-      const unsubscribe4 = st.subscribe(obs4, 'obs4');
-      st.{{name}}.setBooleanField('someBoolean', true);
-      expect(obs1).toBeCalledTimes(1);
-      expect(obs2).toBeCalledTimes(1);
-      expect(obs3).toBeCalledTimes(1);
-      expect(obs4).toBeCalledTimes(1);
-      unsubscribe1();
-      unsubscribe2();
-      st.{{name}}.setBooleanField('someBoolean', false);
-      expect(obs1).toBeCalledTimes(1);
-      expect(obs2).toBeCalledTimes(1);
-      expect(obs3).toBeCalledTimes(2);
-      expect(obs4).toBeCalledTimes(2);
-      unsubscribe1(); // should do no harm
-      unsubscribe4();
-      st.{{name}}.setBooleanField('someBoolean', true);
-      expect(obs1).toBeCalledTimes(1);
-      expect(obs2).toBeCalledTimes(1);
-      expect(obs3).toBeCalledTimes(3);
-      expect(obs4).toBeCalledTimes(2);
-      unsubscribe3();
-      st.{{name}}.setBooleanField('someBoolean', false);
-      expect(obs1).toBeCalledTimes(1);
-      expect(obs2).toBeCalledTimes(1);
-      expect(obs3).toBeCalledTimes(3);
-      expect(obs4).toBeCalledTimes(2);
+      store.{{name}}.setStringField('role', 'TESTER');
+      expect(temporaryObserver).toBeCalledTimes(1);
+      expect(observer).toBeCalledTimes(4);
     });
   });
 
   describe('reset', () => {
     it('clears all state and notifies observers', () => {
-      store.{{name}}.setBooleanField('someBoolean', true);
-      expect(notifier.counter).toBe(1);
       store.reset();
-      expect(store.{{name}}.state).toEqual(refState);
-      expect(notifier.counter).toBe(2);
+      expect(observer).toBeCalledTimes(5);
     });
 
     it('clears all state but does not notify observers', () => {
-      store.{{name}}.setBooleanField('someBoolean', true);
-      expect(notifier.counter).toBe(1);
       store.reset(false);
-      expect(store.{{name}}.state).toEqual(refState);
-      expect(notifier.counter).toBe(1);
+      expect(observer).toBeCalledTimes(5);
     });
   });
 });
