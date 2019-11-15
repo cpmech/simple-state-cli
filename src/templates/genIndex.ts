e715d3586bd4e7860de6d49ca2ce4724cd7c68f3
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-11-15 15:28:27 +1000

    Improve gen index

--- src/templates/genIndex.ts
+++ src/templates/genIndex.ts
@@ -1,7 +1,11 @@
-const template = `export * from './Store';
-import { Store } from './Store';
+const template = `import { Store } from './Store';
+export * from './Store';
 
+/////////////////////////////////
+// global store /////////////////
 export const store = new Store();
+/////////////////////////////////
+/////////////////////////////////
 `;
 
 export const genIndex = (): string => {
